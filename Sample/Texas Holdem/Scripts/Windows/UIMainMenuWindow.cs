4d213a114824a65ae49b42d87e0fc4bb403400df
Rotanticu <1410722798@qq.com>
2025-05-29 12:54:57 +0800

    引用DoTween插件，为动画驱动器具体实现写了预研代码

--- Sample/Texas Holdem/Scripts/Windows/UIMainMenuWindow.cs
+++ Sample/Texas Holdem/Scripts/Windows/UIMainMenuWindow.cs
@@ -6,6 +6,8 @@ using UnityEngine.Playables;
 using UnityEngine.Animations;
 using R3;
 using R3.Triggers;
+using LitMotion;
+using LitMotion.Extensions;
 
 public class UIMainMenuWindow : ASUIWindowBase
 {
@@ -15,27 +17,49 @@ public class UIMainMenuWindow : ASUIWindowBase
     }
     public override void OnShow()
     {
-        PlayableDirector playableDirector = this.GameObject.GetComponent<PlayableDirector>();
-        PlayableGraph graph = PlayableGraph.Create("MainMenuScale");
-        AnimationClip clip = new AnimationClip();
-        clip.frameRate = 60;
-        clip.wrapMode = WrapMode.Once;
-        clip.name = "MainMenuScale";
-        clip.SetCurve("tex_title", typeof(Transform), "localScale.x", new AnimationCurve(new Keyframe(0f, 0.2f), new Keyframe(10f, 1f)));
-        clip.SetCurve("tex_title", typeof(Transform), "localScale.y", new AnimationCurve(new Keyframe(0f, 0.2f), new Keyframe(5f, 1f)));
-        clip.SetCurve("tex_title", typeof(Transform), "localScale.z", new AnimationCurve(new Keyframe(0f, 0.2f), new Keyframe(3f, 1f)));
-        AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(graph, clip);
-        var output = AnimationPlayableOutput.Create(graph, "AnimationOutput", this.GameObject.AddComponent<Animator>());
-        output.SetSourcePlayable(clipPlayable);
-        graph.Play();
-        this.GameObject.transform.OnDestroyAsObservable().Subscribe(_ => graph.Destroy());
-        // int count = playableDirector.playableGraph.GetPlayableCount();
-        // Debug.Log(count);
-        // for (int i = 0; i < count; i++)
-        // {
-        //     Debug.Log(playableDirector.playableGraph.GetRootPlayable(i).GetHandle().ToString());
-        //     //playableDirector.playableGraph.GetRootPlayable(i).SetSpeed(0.5);
-        // }
+        // 使用DOTween实现动画
+        //这部分相当于AnimBuilder
+        // DOTweenAnimation dotweenAnimation = this.GameObject.AddComponent<DOTweenAnimation>();
+        // dotweenAnimation.animationType = DOTweenAnimation.AnimationType.Scale;
+        // dotweenAnimation.targetType = DOTweenAnimation.TargetType.RectTransform;
+        // dotweenAnimation.targetIsSelf = false;
+        // dotweenAnimation.target = this.Transform.Find("tex_title").GetComponent<RectTransform>();
+        // dotweenAnimation.targetGO = dotweenAnimation.target.gameObject;
+        // dotweenAnimation.isValid = true;
+        // dotweenAnimation.duration = 1f;
+        // dotweenAnimation.isFrom = true;
+        // dotweenAnimation.endValueV3 = new Vector3(0.2f, 0.2f, 1f);
+        // //RecreateTween会让当前状态直接成为动画的初始状态，这点可能需要修改
+        // dotweenAnimation.RecreateTween();
+        // //dotweenAnimation.tween相当于AnimProgress
+        // dotweenAnimation.tween.Play();
+
+
+        // 使用LitMotion实现动画
+        //相当于AnimBuilder
+        //var builder = LMotion.Create(new Vector3(0.2f, 0.2f, 1f), new Vector3(1f, 1f, 1f), 1f);
+        //相当于AnimProgress
+        //var motionHandle = builder.BindToLocalScale(this.Transform.Find("tex_title").GetComponent<RectTransform>());
+
+        // 使用PlayableDirector实现动画
+        //playableDirector相当于AnimBuilder
+        // PlayableDirector playableDirector = this.GameObject.GetComponent<PlayableDirector>();
+        // PlayableGraph graph = PlayableGraph.Create("MainMenuScale");
+        // AnimationClip clip = new AnimationClip();
+        // clip.frameRate = 60;
+        // clip.wrapMode = WrapMode.Once;
+        // clip.name = "MainMenuScale";
+        // clip.SetCurve("tex_title", typeof(Transform), "localScale.x", new AnimationCurve(new Keyframe(0f, 0.2f), new Keyframe(1f, 1f)));
+        // clip.SetCurve("tex_title", typeof(Transform), "localScale.y", new AnimationCurve(new Keyframe(0f, 0.2f), new Keyframe(1f, 1f)));
+        // clip.SetCurve("tex_title", typeof(Transform), "localScale.z", new AnimationCurve(new Keyframe(0f, 1f), new Keyframe(1f, 1f)));
+        // AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(graph, clip);
+        // var output = AnimationPlayableOutput.Create(graph, "AnimationOutput", this.GameObject.AddComponent<Animator>());
+        // output.SetSourcePlayable(clipPlayable);
+        // //graph相当于AnimProgress
+        // graph.Play();
+        // this.GameObject.transform.OnDestroyAsObservable().Subscribe(_ => graph.Destroy());
+
+
     }
     public override void PlayShowAnimation()
     {
